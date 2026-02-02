267141fec041c8483b70915c4eb88f63c4ad1aa2
Emre Yalçın <yalcnnemre@gmail.com>
2026-02-02 10:37:24 +0300

    chore: update comment in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-/* sadasd asd*/
+/* sadasd asd asdasd*/
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
